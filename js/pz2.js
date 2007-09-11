a4486dfad009fb0ce5a7a3880cb38be1562d89a8
Jakub Skoczen <jakub@indexdata.dk>
2007-09-11 15:52:13 +0000

    Getting rid of Bug 1364.

--- js/pz2.js
+++ js/pz2.js
@@ -1,5 +1,5 @@
 /*
-** $Id: pz2.js,v 1.54 2007-09-10 13:18:53 jakub Exp $
+** $Id: pz2.js,v 1.55 2007-09-11 15:52:13 jakub Exp $
 ** pz2.js - pazpar2's javascript client library.
 */
 
@@ -816,79 +816,15 @@ Element_getTextContent = function (DOM_Element)
 ** QUERY CLASS ******************************************************************
 *********************************************************************************
 */
-var pzQuery = function()
+
+var pzFilter = function()
 {
-    this.simpleQuery = '';
-    this.singleFilter = null;
-    this.advTerms = new Array();
     this.filterHash = new Array();
-    this.numTerms = 0;
     this.filterNums = 0;
 };
-pzQuery.prototype = {
-    reset: function()
-    {
-        this.simpleQuery = '';
-        this.advTerms = new Array();
-        this.simpleFilter = null;
-        this.numTerms = 0;
-    },
-    clearSimpleQuery: function()
-    {
-        this.simpleQuery = '';
-    },
-    addTerm: function(field, value)
-    {
-        var term = {"field": field, "value": value};
-        this.advTerms[this.numTerms] = term;
-        this.numTerms++;
-    },
-    getTermValueByIdx: function(index)
-    {
-        return this.advTerms[index].value;
-    },
-    getTermFieldByIdx: function(index)
-    {
-        return this.advTerms[index].field;
-    },
-    /* semicolon separated list of terms for given field*/
-    getTermsByField: function(field)
-    {
-        var terms = '';
-        for(var i = 0; i < this.advTerms.length; i++)
-        {
-            if( this.advTerms[i].field == field )
-                terms = terms + this.queryHas[i].value + ';';
-        }
-        return terms;
-    },
-    addTermsFromList: function(inputString, field)
-    {
-        var inputArr = inputString.split(';');
-        for(var i=0; i < inputArr.length; i++)
-        {
-            if(inputArr[i].length < 3) continue;
-            this.advTerms[this.numTerms] = {"field": field, "value": inputArr[i] };
-            this.numTerms++;
-        }
-    },
-    removeTermByIdx: function(index)
-    {
-        this.advTerms.splice(index, 1);
-        this.numTerms--;
-    },
-    toCCL: function()
-    {   
-        var ccl = '';
-        if( this.simpleQuery != '')
-            ccl = this.simpleQuery;
-        for(var i = 0; i < this.advTerms.length; i++)
-        {
-            if (ccl != '') ccl = ccl + ' and ';
-            ccl = ccl + this.advTerms[i].field+'="'+this.advTerms[i].value+'"';
-        }
-        return ccl;
-    },
+
+pzFilter.prototype = 
+{
     addFilter: function(name, value)
     {
         var filter = {"name": name, "id": value };
@@ -896,37 +832,39 @@ pzQuery.prototype = {
         this.filterNums++
         return  this.filterHash.length - 1;
     },
+    
     setFilter: function(name, value)
     {
         this.filterHash = new Array();
         this.filterNums = 0;
         this.addFilter(name, value);
     },
+    
     getFilter: function(index)
     {
         return this.filterHash[index].id;
     },
+    
     getFilterName: function(index)
     {
         return this.filterHash[index].name;
     },
+    
     removeFilter: function(index)
     {
         delete this.filterHash[index];
         this.filterNums--;
     },
+    
     clearFilter: function()
     {
         this.filterHash = new Array();
         this.filterNums = 0;
     },
+    
     getFilterString: function()
-    {
-        //temporary
-        if( this.singleFilter != null ) {
-            return 'pz:id='+this.singleFilter.id;
-        } 
-        else if( this.filterNums <= 0 ) {
+    { 
+        if( this.filterNums <= 0 ) {
             return undefined;
         }
 
@@ -938,24 +876,125 @@ pzQuery.prototype = {
             filter += this.filterHash[i].id; 
         }
         return filter;
+    }
+};
+
+var mkState = function()
+{
+    this.sort = 'relevance';
+    this.perPage = 20;
+    this.page = 0;
+    this.simpleQuery = '';
+    this.singleFilter = null;
+    this.advTerms = new Array();
+    this.numTerms = 0;
+    this.action = '';
+};
+
+mkState.prototype = 
+{
+    reset: function()
+    {
+        this.simpleQuery = '';
+        this.advTerms = new Array();
+        this.simpleFilter = null;
+        this.numTerms = 0;
     },
+    
+    clearSimpleQuery: function()
+    {
+        this.simpleQuery = '';
+    },
+    
+    addTerm: function(field, value)
+    {
+        var term = {"field": field, "value": value};
+        this.advTerms[this.numTerms] = term;
+        this.numTerms++;
+    },
+    
+    getTermValueByIdx: function(index)
+    {
+        return this.advTerms[index].value;
+    },
+    
+    getTermFieldByIdx: function(index)
+    {
+        return this.advTerms[index].field;
+    },
+    
+    /* semicolon separated list of terms for given field*/
+    getTermsByField: function(field)
+    {
+        var terms = '';
+        for(var i = 0; i < this.advTerms.length; i++)
+        {
+            if( this.advTerms[i].field == field )
+                terms = terms + this.queryHas[i].value + ';';
+        }
+        return terms;
+    },
+    
+    addTermsFromList: function(inputString, field)
+    {
+        var inputArr = inputString.split(';');
+        for(var i=0; i < inputArr.length; i++)
+        {
+            if(inputArr[i].length < 3) continue;
+            this.advTerms[this.numTerms] 
+                = {"field": field, "value": inputArr[i] };
+            this.numTerms++;
+        }
+    },
+    
+    removeTermByIdx: function(index)
+    {
+        this.advTerms.splice(index, 1);
+        this.numTerms--;
+    },
+    
+    toCCL: function()
+    {   
+        var ccl = '';
+        if( this.simpleQuery != '')
+            ccl = this.simpleQuery;
+        for(var i = 0; i < this.advTerms.length; i++)
+        {
+            if (ccl != '') ccl = ccl + ' and ';
+            ccl = ccl + this.advTerms[i].field
+                +'="'+this.advTerms[i].value+'"';
+        }
+        return ccl;
+    },
+    
     totalLength: function()
     {
         var simpleLength = this.simpleQuery != '' ? 1 : 0;
         return this.advTerms.length + simpleLength;
     },
+    
     clearSingleFilter: function()
     {
         this.singleFilter = null;
     },
+    
     setSingleFilter: function(name, value)
     {
         this.singleFilter = {"name": name, "id": value };
     },
+    
     getSingleFilterName: function()
     {
         return this.singleFilter.name;
-    }
-}
+    },
 
+    getSingleFilterString: function()
+    {
+        if( this.singleFilter != null ) {
+            return 'pz:id='+this.singleFilter.id;
+        } else {
+            return undefined;
+        }
+    }
+};
 }
