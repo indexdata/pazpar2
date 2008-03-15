b4e80d145253134b44614896ba7f0b1975d22afa
Adam Dickmeiss <adam@indexdata.dk>
2008-03-15 22:26:38 +0100

    Auto add Git submodules in buildconf.sh.

--- buildconf.sh
+++ buildconf.sh
@@ -8,6 +8,12 @@ libtoolize=libtoolize
 autoheader=autoheader
 
 test -d config || mkdir config
+if test -d m4/.git -a -d doc/common/.git; then
+    :
+else
+    git submodule init
+    git submodule update
+fi
 
 if [ "`uname -s`" = FreeBSD ]; then
     # FreeBSD intalls the various auto* tools with version numbers
