ef1c75777701370746b319868eae804d32555218
Adam Dickmeiss <adam@indexdata.dk>
2008-06-20 12:42:02 +0200

    Removed unused debian template directory.

--- debian.dh_make/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pazpar2 package
-#
-0 4	* * *	root	pazpar2_maintenance
--- debian.dh_make/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pazpar2):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="pazpar2" command="/usr/bin/pazpar2"
