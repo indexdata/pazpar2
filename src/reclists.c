32946760b9c974f112017d79ab756cf1dd85ca6b
Adam Dickmeiss <adam@indexdata.dk>
2009-09-08 14:23:35 +0200

    Avoid extern when a header just does it

--- src/reclists.c
+++ src/reclists.c
@@ -27,8 +27,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "pazpar2.h"
 #include "reclists.h"
-
-extern struct parameters global_parameters;
+#include "parameters.h"
 
 static struct reclist_sortparms *qsort_sortparms = 0; /* thread pr */
 
