e11fb51035f2748092beef2cacb3171bac40bd41
Adam Dickmeiss <adam@indexdata.dk>
2009-09-29 15:20:15 +0200

    Change proto to use const char ptr

--- src/marcmap.c
+++ src/marcmap.c
@@ -37,7 +37,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "marcmap.h"
 #include "marchash.h"
 
-struct marcmap *marcmap_load(char *filename, NMEM nmem) {
+struct marcmap *marcmap_load(const char *filename, NMEM nmem)
+{
     struct marcmap *mm;
     struct marcmap *mmhead;
     FILE *fp;
