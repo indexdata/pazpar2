49873e8213488678ae95c0789aff12462f80d181
Adam Dickmeiss <adam@indexdata.dk>
2009-10-14 16:32:24 +0200

    Test case for setting overrides

---
+++ test/test_settings.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+
+# srcdir might be set by make
+srcdir=${srcdir:-"."}
+
+# Test using test_http.cfg
+exec ${srcdir}/run_pazpar2.sh test_settings
+
+# Local Variables:
+# mode:shell-script
+# sh-indentation: 2
+# sh-basic-offset: 4
+# End:
