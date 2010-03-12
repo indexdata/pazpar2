098b558355b375d17f6fff413b836b2ae8438e65
Adam Dickmeiss <adam@indexdata.dk>
2010-03-12 16:20:12 +0100

    More tuning for bash-based perf test

--- perf/bash/par.sh
+++ perf/bash/par.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-DELAY=0.1
-NUMBER=10
-let i=0
-while test $i -lt $NUMBER; do
-	./client.sh $i >$i.log 2>&1 &
-	sleep $DELAY
-	let i=$i+1
+DELAY=0.001
+NUMBER=40
+ROUNDS=5
+let r=0
+while test $r -lt $ROUNDS; do
+	echo "$r"
+	let i=0
+	while test $i -lt $NUMBER; do
+		./client.sh $r.$i >$r.$i.log 2>&1 &
+		sleep $DELAY
+		let i=$i+1
+	done
+	let r=$r+1
 done
+wait
+wget -O x 'http://localhost:9004/?command=exit'
