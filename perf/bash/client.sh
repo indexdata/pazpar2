3f5d30f1d63add1aaada59c1ec2076a0c3901c6a
Dennis Schafroth <dennis@indexdata.com>
2010-03-17 13:52:16 +0100

    Added port parameter and block=1

--- perf/bash/client.sh
+++ perf/bash/client.sh
@@ -3,9 +3,16 @@ OF=$1
 if test -z "$OF"; then
 	OF=1
 fi
-H='http://localhost:9004/search.pz2'
-wget -q -O $OF.init.xml "$H/?command=init&service=perf&extra=$OF"
+
+PORT=$2
+if test -z "$PORT"; then
+	PORT=9004
+fi
+
+
+H="http://localhost:${PORT}/search.pz2"
+wget -q -O $OF.init.xml "$H/?command=init&service=perf_t&extra=$OF"
 S=`xsltproc get_session.xsl $OF.init.xml`
 wget -q -O $OF.search.xml "$H?command=search&query=100&session=$S"
-sleep 0.5
-wget -q -O $OF.show.xml "$H?command=show&session=$S"
+sleep 1
+wget -q -O $OF.show.xml "$H?command=show&session=$S&num=100&block=1"
