82b1775c39e27ad3cb01e9f045f916bfb1b14338
Dennis Schafroth <dennis@indexdata.com>
2010-03-17 13:54:55 +0100

    enabled easier switch of client script

--- perf/bash/par_fixed_clients.sh
+++ perf/bash/par_fixed_clients.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+NUMBER=$1
+if [ -z "$NUMBER" ] ; then
+    NUMBER=20
+fi
+
 DELAY=0.001
 WAIT=1
-NUMBER=100
-ROUNDS=5
+#NUMBER=100
+ROUNDS=2
 let r=0
+PORT=9005
+CLIENT_SCRIPT="client_timed.sh"
 while test $r -lt $ROUNDS; do
 	echo "$r"
 	i=0
 	while test $i -lt $NUMBER; do
-		./client.sh $r.$i >$r.$i.log 2>&1 &
-		CLIENTS=`ps -ef |grep -c client.sh` 
+		./${CLIENT_SCRIPT} $r.$i $PORT >$r.$i.log 2>&1 &
+		CLIENTS=`ps -ef |grep -c ${CLIENT_SCRIPT}` 
 		while test $CLIENTS -ge $NUMBER ; do
 		    sleep $WAIT
-		    CLIENTS=`ps -ef |grep -c client.sh` 
+		    CLIENTS=`ps -ef |grep -c ${CLIENT_SCRIPT}` 
 		    echo "Active $CLIENTS"
 		done
 		let i=$i+1
@@ -20,4 +27,6 @@ while test $r -lt $ROUNDS; do
 	let r=$r+1
 done
 wait
-wget -O x 'http://localhost:8010/?command=exit'
+cat *.time > timed.$NUMBER.log
+#wget --tries=1 -O x "http://localhost:${PORT}/?command=exit"
+sleep 5
