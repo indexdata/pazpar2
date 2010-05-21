e006b6036fb2947abe8db08881bb758ffd77d72c
Dennis Schafroth <dennis@indexdata.com>
2010-05-21 11:51:42 +0200

    Support for settings. Default users is 5.

--- perf/bash/par.sh
+++ perf/bash/par.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DELAY=0.001
 WAIT=5
-NUMBER=10
+NUMBER=5
 ROUNDS=5
 PORT=9004
 SERVICE=perf_t
@@ -14,7 +14,7 @@ while test $r -lt $ROUNDS; do
 	echo "$r"
 	let i=0
 	while test $i -lt $NUMBER; do
-		./client.sh $r.$i $PORT $SERVICE >$r.$i.log 2>&1 &
+		./client.sh $r.$i $PORT $SERVICE $SETTINGS >$r.$i.log 2>&1 &
 		sleep $DELAY
 		let i=$i+1
 	done
