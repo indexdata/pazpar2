3b76133214c2f00a462f094459bf07934bf4bb44
Dennis Schafroth <dennis@indexdata.com>
2010-07-23 12:40:50 +0200

    Client requesting termlist/facets

---
+++ perf/bash/client_termlist.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+OF=$1
+if test -z "$OF"; then
+	OF=1
+fi
+
+PORT=$2
+if test -z "$PORT"; then
+	PORT=9004
+fi
+SERVICE=$3
+if test -z "$SERVICE"; then
+	SERVICE=perf
+fi
+SETTINGS=$4
+WHAT=water
+H="http://localhost:${PORT}/search.pz2"
+wget -q -O $OF.init.xml "$H/?command=init&service=${SERVICE}&extra=$OF"
+S=`xsltproc get_session.xsl $OF.init.xml`
+if [ "$SETTINGS" != "" ] ; then
+    wget -q -O $OF.settings.xml "$H?command=settings&session=$S&$SETTINGS"
+fi
+wget -q -O $OF.search.xml "$H?command=search&query=$WHAT&session=$S"
+sleep 1
+#wget -q -O $OF.show.xml "$H?command=termlist&session=$S&sort=relevance&start=0&num=100&block=1"
+wget -q -O $OF.termlist.xml "$H?command=termlist&name=xtargets%2Csubject%2Cauthor&session=$S"
