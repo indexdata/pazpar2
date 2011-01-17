ee50fa90d3c4da5868d62182d4c8195491390b67
Dennis Schafroth <dennis@indexdata.com>
2011-01-17 16:30:57 +0100

    Fix for bug 3948.

--- src/settings.c
+++ src/settings.c
@@ -73,6 +73,7 @@ static char *hard_settings[] = {
     "pz:reuse_connections",     /* PZ_REUSE_CONNECTION    */
     "pz:termlist_term_sort",    /* PZ_TERMLIST_TERM_SORT  */
     "pz:termlist_term_count",   /* PZ_TERMLIST_TERM_COUNT */
+    "pz:termlist_term_factor",  /* PZ_TERMLIST_TERM_FACTOR*/
     "pz:preferred",             /* PZ_PREFERRED           */
     0
 };
