8a659d234585883a266aa1c086254098122b2b92
Dennis Schafroth <dennis@indexdata.com>
2011-02-07 15:41:42 +0100

    Test script for jinx. Should be moved.

---
+++ jinx-it.sh
@@ -0,0 +1,8 @@
+if [ "$1" == "" ] ; then 
+    COUNT=10
+else
+    COUNT=$1
+fi
+for d in `seq $COUNT` ; do 
+    jinx run src/pazpar2 -X -f ~/etc/pazpar2/server-threaded.xml -l pazpar2.log -v all
+done 
