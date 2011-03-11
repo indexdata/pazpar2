69171b282699fa3f8fa2a03ffd3c5b79abeb619b
Dennis Schafroth <dennis@indexdata.com>
2011-03-11 14:15:49 +0100

    Support for settings

--- perf/bash/par.sh
+++ perf/bash/par.sh
@@ -11,13 +11,20 @@ if test -n "$1"; then
 	. $1
 fi
 let r=0
+if [ "$SETTINGS" != "" ] ; then 
+    SETTINGS_OPT="--settings=\"$SETTINGS\" " 
+    echo $SETTINGS_OPT
+else
+    unset SETTINGS_OPT
+fi
+
 while test $r -lt $ROUNDS; do
 	echo "$r"
 	let i=0
 	while test $i -lt $NUMBER; do
-		./pp2client.sh --outfile=$r.$i --prefix=http://$HOST:${PORT}/search.pz2 --service=$SERVICE >$r.$i.log 2>&1 &
-		sleep $DELAY
-		let i=$i+1
+	    ./pp2client.sh --outfile=$r.$i --prefix=http://$HOST:${PORT}/search.pz2 --service=$SERVICE $SETTINGS_OPT >$r.$i.log 2>&1 &
+	    sleep $DELAY
+	    let i=$i+1
 	done
 	sleep $WAIT
 	let r=$r+1
