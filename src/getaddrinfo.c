02d06913705a0285b11b570e51d2f1193ff971c1
Adam Dickmeiss <adam@indexdata.dk>
2013-04-02 16:03:23 +0200

    Add ws2tcpip.h for getaddrinfo on Windows

--- src/getaddrinfo.c
+++ src/getaddrinfo.c
@@ -34,7 +34,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <sys/socket.h>
 #endif
 #ifdef WIN32
-#include <winsock.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #endif
 #if HAVE_NETDB_H
 #include <netdb.h>
