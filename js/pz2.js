6e5054c11a4470dde3515a362aa3f38d69df68d0
Mike Taylor <mike@indexdata.com>
2014-10-29 10:20:17 +0000

    Bug PAZ-978.
    Remove code that translates just two diagnostic codes (1 and 2) into
    human-readable messages. This is no longer necessary at all, as
    PazPar2 has been returning the message as well as the code in its
    responses since December 2012.

--- js/pz2.js
+++ js/pz2.js
@@ -709,11 +709,6 @@ pz2.prototype =
                         } else if (bytarget[i]["state"]=="Client_Working") {
                           bytarget[i]["hits"] = "...";
                         }
-                        if (bytarget[i].diagnostic == "1") {
-                          bytarget[i].diagnostic = "Permanent system error";
-                        } else if (bytarget[i].diagnostic == "2") {
-                          bytarget[i].diagnostic = "Temporary system error";
-                        } 
                         var targetsSuggestions = targetNodes[i].getElementsByTagName("suggestions");
                         if (targetsSuggestions != undefined && targetsSuggestions.length>0) {
                           var suggestions = targetsSuggestions[0];
