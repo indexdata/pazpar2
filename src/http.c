indexdata/pazpar2

commit 3711bf09aa1125b5277926c29cc0526df68194ac

Flush HTTP recording file

src/http.c
@@ -926,6 +926,7 @@ static void http_io(IOCHAN i, int event)
                             (long long) iochan_getfd(i), sz);
                     for (hb = hc->iqueue; hb; hb = hb->next)
                         fwrite(hb->buf, 1, hb->len, hc->http_server->record_file);
+                    fflush(hc->http_server->record_file);
                 }
  #endif
                 if (!(hc->request = http_parse_request(hc, &hc->iqueue, reqlen)))
@@ -979,6 +980,8 @@ static void http_io(IOCHAN i, int event)
                                 (long long) iochan_getfd(i), sz);
                         fwrite(wb->buf, 1, wb->offset + wb->len,
                                hc->http_server->record_file);
+                        fputc('\n', hc->http_server->record_file);
+                        fflush(hc->http_server->record_file);
                     }
  #endif
                     hc->oqueue = hc->oqueue->next;

commit 474aa2bb93c17cf82653574c8c8632e9907419bc

Fix pazpar2 recording broken PAZ-1029
Bug was introduced in 3711bf09aa1125b5277926c29cc0526df68194ac.

src/http.c
@@ -1001,7 +1001,6 @@ static void http_io(IOCHAN i, int event)
                                 (long long) iochan_getfd(i), sz);
                         fwrite(wb->buf, 1, wb->offset + wb->len,
                                hc->http_server->record_file);
-                        fputc('\n', hc->http_server->record_file);
                         fflush(hc->http_server->record_file);
                     }
  #endif